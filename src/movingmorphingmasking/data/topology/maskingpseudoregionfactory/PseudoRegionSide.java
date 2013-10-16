45cf1c6471c960592cc0131ac8477688c183e480
schueler87 <julia.schueler@uni-greifswald.de>
2013-10-16 13:54:34 +0200

    Adds in PseudoRegionSide method to get the longest PseudoRegionSide between two PseudoRegionSides.

--- src/movingmorphingmasking/data/topology/maskingpseudoregionfactory/PseudoRegionSide.java
+++ src/movingmorphingmasking/data/topology/maskingpseudoregionfactory/PseudoRegionSide.java
@@ -38,8 +38,8 @@ public class PseudoRegionSide {
 
     /**
      * Constructor for a
-     * <code>PseudoRegionSide</code> with the start point (x1,y1) and end point (x2,
-     * y2).
+     * <code>PseudoRegionSide</code> with the start point (x1,y1) and end point
+     * (x2, y2).
      *
      * @param x1 X Coordinate of the start point
      * @param y1 Y Coordinate of the start point
@@ -101,6 +101,68 @@ public class PseudoRegionSide {
     }
 
     /**
+     * Returns if a specific side is on the same straight line as this.
+     *
+     * @param side specific side to check.
+     * @return
+     */
+    public boolean isOnSameStraightLine(PseudoRegionSide side) {
+        return isOnSameStraightLine(side.x1, side.y1, side.x2, side.y2);
+    }
+
+    /**
+     * Returns if a specific side (defined by (x1,y1)->(x2,y2) is on the same
+     * straight line as this.
+     *
+     * @param x1 the X coordinate of the start point of a specific side.
+     * @param y1 the Y coordinate of the start point of a specific side.
+     * @param x2 the X coordinate of the end point of a specific side.
+     * @param y2 the Y coordinate of the end point of a specific side.
+     * @return
+     */
+    public boolean isOnSameStraightLine(double x1, double y1, double x2, double y2) {
+        if (isEqualSide(x1, y1, x2, y2)) {
+            return true;
+        }
+        PseudoRegionSide side = getLongestPseudoRegionSide(new PseudoRegionSide(x1, y1, x2, y2));
+        return (side.isOnSide(x1, y1) && side.isOnSide(x2, y2)
+                && side.isOnSide(this.x1, this.y1) && side.isOnSide(this.x2, this.y2));
+    }
+
+    /**
+     * Returns a
+     * <code>PeudoRegionSide</code> of this and a specific side. With adding the
+     * points on side and this and adding the associated polygons. It this and a
+     * specific side are not a the same straight line, then it returns a
+     * <code>PeudoRegionSide</code> as a line that connects the points on both
+     * sides with longest distance to each other.
+     *
+     * @param side specific side.
+     * @return longest possible side between this and a specific side.
+     */
+    public PseudoRegionSide getConnectedPseudoRegionSide(PseudoRegionSide side) {
+        PseudoRegionSide longestPseudoRegionSide = getLongestPseudoRegionSide(side);
+        for (Point2D point2D : pointsOnSide) {
+            longestPseudoRegionSide.addPoint(point2D.getX(), point2D.getY());
+        }
+        for (Point2D point2D : side.pointsOnSide) {
+            longestPseudoRegionSide.addPoint(point2D.getX(), point2D.getY());
+        }
+        for (PolygonSimple polygonSimple : associatedPolygons) {
+            if (!longestPseudoRegionSide.associatedPolygons.contains(polygonSimple)) {
+                longestPseudoRegionSide.addAssociatedPolygon(polygonSimple);
+            }
+        }
+        for (PolygonSimple polygonSimple : side.associatedPolygons) {
+            if (!longestPseudoRegionSide.associatedPolygons.contains(polygonSimple)) {
+                longestPseudoRegionSide.addAssociatedPolygon(polygonSimple);
+            }
+        }
+        longestPseudoRegionSide.sortPointsOnSide();
+        return longestPseudoRegionSide;
+    }
+
+    /**
      * Adds a specific point if it is on this side and not already added.
      *
      * @param x X Coordinate of the specific point.
@@ -108,8 +170,11 @@ public class PseudoRegionSide {
      * @return <tt>true</tt>
      */
     public boolean addPoint(double x, double y) {
+        System.out.println("Side " + x1 + " " + y1 + " -> " + x2 + " " + y2);
+        System.out.println(" look at " + x + " " + y + " " + isOnSide(x, y));
         if (isOnSide(x, y) && indexOfPoint(x, y) < 0) {
             pointsOnSide.add(new Point2D.Double(x, y));
+            System.out.println("   can add it");
             return true;
         }
         return false;
@@ -117,8 +182,7 @@ public class PseudoRegionSide {
 
     /**
      * Returns if a a specific point (x,y) is on this side. First it checks
-     * first the relative CCW is equals 0
-     * (
+     * first the relative CCW is equals 0 (
      *
      * @see as specified by {@link Line2D#relativeCCW})). Then it checks, if the
      * specific point is between the start point and end point of this.
@@ -140,8 +204,7 @@ public class PseudoRegionSide {
     }
 
     /**
-     * Returns if a a specific point (x,y) is on this side 
-     * (
+     * Returns if a a specific point (x,y) is on this side (
      *
      * @see as specified by {@link PseudoRegionSide#isOnSide})).
      * @param x1 X Coordinate of the start point of specific side.
@@ -158,7 +221,7 @@ public class PseudoRegionSide {
 
     /**
      * Returns the position of a specific point(x,y) on this side. It returns -1
-     * if the point is not an added point (@link PseudoRegionSide#addPoint), 
+     * if the point is not an added point (@link PseudoRegionSide#addPoint),
      * or start point or a end point of this.
      * (NOTE: use before @link PseudoRegionSide#sortPointsOnSide).
      *
@@ -167,7 +230,8 @@ public class PseudoRegionSide {
      * @return position of a specific point.
      */
     public int indexOfPoint(double x, double y) {
-        for (int i = 0; i < pointsOnSide.size(); i++) {
+        int n = pointsOnSide.size();
+        for (int i = 0; i < n; i++) {
             Point2D point2D = pointsOnSide.get(i);
             if (point2D.getX() == x && point2D.getY() == y) {
                 return i;
@@ -177,27 +241,31 @@ public class PseudoRegionSide {
     }
 
     /**
-     * Returns all the <code>Point2D</code> included the specific
-     * start point (x1, y1) and specific end point (x2, y2) and all
-     * added points (@link PseudoRegionSide#addPoint) between them.
+     * Returns all the
+     * <code>Point2D</code> included the specific start point (x1, y1) and
+     * specific end point (x2, y2) and all added points (@link PseudoRegionSide#addPoint) between them.
      * (NOTE: use before @link PseudoRegionSide#sortPointsOnSide).
-     * 
+     *
      * It returns an empty list, if the specific start point or end point is
      * not an start or end point of this or an added point (@link PseudoRegionSide#addPoint).
-     * 
+     *
      * @param x1
      * @param y1
      * @param x2
      * @param y2
-     * @return 
+     * @return
      */
     public List<Point2D> getPointsOnSideBetween(double x1, double y1, double x2, double y2) {
-        ArrayList<Point2D> points = new ArrayList<Point2D>();
+        List<Point2D> points = new ArrayList<Point2D>();
         int index_1 = indexOfPoint(x1, y1);
         int index_2 = indexOfPoint(x2, y2);
+        int n = pointsOnSide.size();
+        System.out.println("index 1 " + index_1 + " index_2 " + index_2 + " n " + n);
         if (index_1 < 0 || index_2 < 0 || index_1 == index_2) {
             return points;
         }
+        index_1 = Math.min(n - 1, Math.max(0, index_1));
+        index_2 = Math.min(n - 1, Math.max(0, index_2));
         if (index_1 < index_2) {
             for (int i = index_1; i <= index_2; i++) {
                 points.add(pointsOnSide.get(i));
@@ -213,10 +281,43 @@ public class PseudoRegionSide {
 
     /**
      * Sorted the added point, including start and end point of this, according
-     * to the squarified distance to the start point of this
-     * using the <code>DistanceToPointComparator</code>.
+     * to the squarified distance to the start point of this using the
+     * <code>DistanceToPointComparator</code>.
      */
     public void sortPointsOnSide() {
         Collections.sort(pointsOnSide, new DistanceToPointComparator(x1, y1));
+    }
+
+    @Override
+    public String toString() {
+        return x1 + " " + y1 + " --> " + x2 + " " + y2;
+    }
+
+    private PseudoRegionSide getLongestPseudoRegionSide(PseudoRegionSide side) {
+        double d1 = Point2D.distanceSq(x1, y1, x2, y2);
+        double d2 = Point2D.distanceSq(side.x1, side.y1, side.x2, side.y2);
+        double d3 = Point2D.distanceSq(side.x1, side.y1, x2, y2);
+        double d4 = Point2D.distanceSq(side.x1, side.y1, x1, y1);
+        double d5 = Point2D.distanceSq(side.x2, side.y2, x2, y2);
+        double d6 = Point2D.distanceSq(side.x2, side.y2, x1, y1);
+        if (d1 >= d2 && d1 >= d3 && d1 >= d4 && d1 >= d5 && d1 >= d6) {
+            return this;
+        }
+        if (d2 >= d3 && d2 >= d4 && d2 >= d5 && d2 >= d6) {
+            return side;
+        }
+        if (d3 >= d4 && d3 >= d5 && d3 >= d6) {
+            return new PseudoRegionSide(side.x1, side.y1, x2, y2);
+        }
+        if (d4 >= d5 && d4 >= d6) {
+            return new PseudoRegionSide(side.x1, side.y1, x1, y1);
+        }
+        if (d5 >= d6) {
+            return new PseudoRegionSide(side.x2, side.y2, x2, y2);
+        }
+        return new PseudoRegionSide(side.x2, side.y2, x1, y1);
+
+
+
     }
 }
