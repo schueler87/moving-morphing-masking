f86dd9cca879546227292ec00534c691719324e0
schueler87 <julia.schueler@uni-greifswald.de>
2013-10-16 13:41:03 +0200

    Use PseudoRegionCornerPoint2DFactory in MapPointCornerFactory.

--- src/movingmorphingmasking/data/topology/maskingpseudoregionfactory/MapPointToCornerFactory.java
+++ src/movingmorphingmasking/data/topology/maskingpseudoregionfactory/MapPointToCornerFactory.java
@@ -8,15 +8,17 @@ import kn.uni.voronoitreemap.j2d.PolygonSimple;
 import movingmorphingmasking.data.topology.CornerPoint2D;
 
 /**
- * Factory for a <code>HashMap</code> the creates for each point for a given tesselation of a
+ * Factory for a
+ * <code>HashMap</code> the creates for each point for a given tesselation of a
  * base polygon a
- * <code>CornerPoint2D</code> and save them in a <code>HashMap</code>.
+ * <code>CornerPoint2D</code> and save them in a
+ * <code>HashMap</code>.
  *
  * @author julia schueler
  */
 public class MapPointToCornerFactory {
 
-    private PolygonSimple basePolygon;
+    private final PseudoRegionCornerPoint2DFactory cornerPoint2DFactory;
 
     private static enum PolygonMode {
 
@@ -26,35 +28,48 @@ public class MapPointToCornerFactory {
     /**
      * Constructor with the base polygon of a tesselation.
      *
-     * @param basePolygon
+     * @param basePolygon base polygon
+     * @param pseudoRegionPolygones tesselation of the base polygon in pseudo
+     * regions.
      */
-    public MapPointToCornerFactory(PolygonSimple basePolygon) {
-        this.basePolygon = basePolygon;
+    public MapPointToCornerFactory(PolygonSimple basePolygon, List<PolygonSimple> pseudoRegionPolygones) {
+        this(new PseudoRegionCornerPoint2DFactory(basePolygon, pseudoRegionPolygones));
     }
 
     /**
-     * Returns a <code>HashMap</code> of a given tesselation and a 
-     * tesselation of the base polygon as pseudo region polygons.
-     * 
-     * The mapped <code>CornerPoint2D</code> is moveable 
-     * (@see specified by {@link CornerPoint2D#MOVABLE}), if it lays on a side
-     * of a pseudo region polygon, if it lays in addition on a base polygon side,
-     * but is not a corner of the base polygon, it is a line slider
-     * (@see specified by {@link CornerPoint2D#LineSlider}). 
-     * Otherwise the <code>CornerPoint2D</code> is immovable
-     * (@see specified by {@link CornerPoint2D#IMMOVABLE}).
-     * 
-     * @param pseudoRegionPolygones tesselation of the base polygon in pseudo
-     * regions.
-     * @param tesselation tesselation of the base polygon.
-     * @return map of a corner of all polygons in the tesselation to a <code>CornerPoint2D</code>.
+     * Constructor with the
+     * <code>CornerPoint2D</code> factory for pseudo region tesselation.
+     *
+     * @param cornerPoint2DFactory <code>CornerPoint2D</code> factory for pseudo
+     * region tesselation.
      */
-    public HashMap<Point2D, CornerPoint2D> createPointToCornerMap(List<PolygonSimple> pseudoRegionPolygones, List<PolygonSimple> tesselation) {
+    public MapPointToCornerFactory(PseudoRegionCornerPoint2DFactory cornerPoint2DFactory) {
+        this.cornerPoint2DFactory = cornerPoint2DFactory;
+    }
+
+    /**
+     * Returns a
+     * <code>HashMap</code> of a given tesselation and a tesselation of the base
+     * polygon as pseudo region polygons.
+     *
+     * The mapped
+     * <code>CornerPoint2D</code> is moveable (
+     *
+     * @see specified by {@link CornerPoint2D#MOVABLE}), if it lays on a side of
+     * a pseudo region polygon, if it lays in addition on a base polygon side,
+     * but is not a corner of the base polygon, it is a line slider (
+     * @see specified by {@link CornerPoint2D#LineSlider}). Otherwise *      * the <code>CornerPoint2D</code> is immovable (
+     * @see specified by {@link CornerPoint2D#IMMOVABLE}).
+     *
+     * @param tesselation tesselation of the base polygon.
+     * @return map of a corner of all polygons in the tesselation to *      * a <code>CornerPoint2D</code>.
+     */
+    public HashMap<Point2D, CornerPoint2D> createPointToCornerMap(List<PolygonSimple> tesselation) {
         HashMap<Point2D, CornerPoint2D> mapPointToCorner = new HashMap<Point2D, CornerPoint2D>();
 
-        addPolygonCorners(basePolygon, mapPointToCorner, PolygonMode.base);
+        addPolygonCorners(cornerPoint2DFactory.getBasePolygon(), mapPointToCorner, PolygonMode.base);
 
-        for (PolygonSimple polygonSimple : pseudoRegionPolygones) {
+        for (PolygonSimple polygonSimple : cornerPoint2DFactory.getPseudoRegionPolygones()) {
             addPolygonCorners(polygonSimple, mapPointToCorner, PolygonMode.pseudo);
         }
 
@@ -74,7 +89,6 @@ public class MapPointToCornerFactory {
         for (int i = 0; i < n; i++) {
             Point2D point = new Point2D.Double(x[i], y[i]);
             addCornerPoint(point, mode, mapPointToCorner);
-
         }
     }
 
@@ -87,7 +101,7 @@ public class MapPointToCornerFactory {
                 break;
             case pseudo:
                 if (!mapPointToCorner.containsKey(point)) {
-                    CornerPoint2D cornerPoint2D = getLineSliderOnBasePolygonSide(x, y);
+                    CornerPoint2D cornerPoint2D = cornerPoint2DFactory.getLineSliderOnBasePolygonSide(x, y);
                     if (cornerPoint2D == null) {
                         cornerPoint2D = new CornerPoint2D.Movable(x, y);
                     }
@@ -96,26 +110,10 @@ public class MapPointToCornerFactory {
                 break;
             case normal:
                 if (!mapPointToCorner.containsKey(point)) {
-                    CornerPoint2D cornerPoint2D = new CornerPoint2D.Immovable(x, y);
+                    CornerPoint2D cornerPoint2D = cornerPoint2DFactory.getCornerPointOnTesselationSide(x, y);
                     mapPointToCorner.put(point, cornerPoint2D);
                 }
                 break;
         }
-    }
-
-    private CornerPoint2D getLineSliderOnBasePolygonSide(double x, double y) {
-        double[] xi = basePolygon.getXPoints();
-        double[] yi = basePolygon.getYPoints();
-        int n = xi.length;
-        for (int i = 0; i < n; i++) {
-            double x1 = xi[i];
-            double y1 = yi[i];
-            double x2 = xi[(i + 1) % n];
-            double y2 = yi[(i + 1) % n];
-            if (PseudoRegionSide.isOnSide(x1, y1, x2, y2, x, y)) {
-                return new CornerPoint2D.LineSlider(x, y, new Line2D.Double(x1, y1, x2, y2));
-            }
-        }
-        return null;
     }
 }
